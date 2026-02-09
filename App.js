0320fde4d778314da7d5185d4a8ad525d5b63b23
somnathdurgade <somnath.s.durgade@gmail.com>
2026-02-09 13:37:07 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Interactive Web App</h1>
+      <p>This is a sample React project showcasing components and state.</p>
+    </div>
+  );
+}
+
+export default App;
